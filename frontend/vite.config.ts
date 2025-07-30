14a0616d0e2972077582bf0f88edac4671a18950
TeaProphet <yefremov.vlad@gmail.com>
2025-07-30 23:03:31 +0300

    Updated vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,4 +19,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    assetsDir: 'app-assets', // или любое другое имя
+  }
 }));
